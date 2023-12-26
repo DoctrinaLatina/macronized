41e54fb89df5f4eb925b12b981a6d99417bf6f41
Michael Reutman <mreutman@gmail.com>
2023-12-25 16:41:03 -0800

    Create subsitutions script

--- substitutions/substitutions.md
+++ substitutions/substitutions.md
@@ -1,6 +1,6 @@
 # Changes
 
-## Intentional Vowel Lengthening
+## mihi/tibi/sibi/ibi/ubi
 It appears that the final 'i' wasn't always short for these words; some uses would lengthen it. We will favor the long vowel ending in order to make pronunciation more "natural" and to find some common ground with modern ecclesiastical pronunciation.
 
 mihi -> mihī
@@ -8,3 +8,8 @@ tibi -> tibī
 sibi -> sibī
 ibi -> ibī
 ubi -> ubī
+
+## amen
+Favor short 'e' to align with pronunciation that became standardized in the first century.
+
+āmēn -> āmen
---
+++ substitutions/substitutions.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -x
+
+function _replace() {
+  sed -i 's/\bmihi\b/mihī/g' $1
+  sed -i 's/\bMihi\b/Mihī/g' $1
+
+  sed -i 's/\btibi\b/tibī/g' $1
+  sed -i 's/\bTibi\b/Tibī/g' $1
+
+  sed -i 's/\bsibi\b/sibī/g' $1
+  sed -i 's/\bSibi\b/Sibī/g' $1
+
+  sed -i 's/\bibi\b/ibī/g' $1
+  sed -i 's/\bIbi\b/Ibī/g' $1
+
+  sed -i 's/\bubi\b/ubī/g' $1
+  sed -i 's/\bUbi\b/Ubī/g' $1
+
+  sed -i 's/\bamen\b/āmen/g' $1
+  sed -i 's/\bāmēn\b/āmen/g' $1
+  sed -i 's/\bĀmēn\b/Āmen/g' $1
+  sed -i 's/\bĀMĒN\b/ĀMEN/g' $1
+}
+
+if [ $# -eq 0 ]; then
+  echo "USAGE: $0 [DIR/FILE]"
+  exit 1
+fi
+
+if test -f $1; then
+  _replace $1
+elif test -d $1; then
+  for F in $1/*; do
+    if test -f $F; then
+      _replace $F
+    fi
+  done
+fi
