1c6826ccd74a9ea0b1a0b6732ecd6ded2bb30b01
Michael Reutman <mreutman@gmail.com>
2024-02-08 13:48:19 -0800

    Add "J" to words in Psalms

---
+++ substitutions/substitution-csv.sh
@@ -0,0 +1,42 @@
+#! /bin/bash
+
+set -e
+set -x
+
+if [ $# -ne 2 ]; then
+  echo "USAGE: $0 <CSV> <FILE/DIR>"
+  exit 0
+fi
+
+function _replace() {
+  CSV=$1
+  INPUT=$2
+  while IFS="," read -r OLDWORD OLDSTEM NEWWORD NEWSTEM
+  do
+    echo "Replacing \"$OLDWORD\" ($OLDSTEM-) with \"$NEWWORD\" ($NEWSTEM-)"
+
+    UPOLDSTEM=${OLDSTEM^}
+    UPNEWSTEM=${NEWSTEM^}
+    CAPSOLDSTEM=${OLDSTEM^^}
+    CAPSNEWSTEM=${NEWSTEM^^}
+
+    sed -i "s/\b$OLDSTEM/$NEWSTEM/g" $INPUT
+    sed -i "s/\b$UPOLDSTEM/$UPNEWSTEM/g" $INPUT
+    sed -i "s/\b$CAPSOLDSTEM/$CAPSNEWSTEM/g" $INPUT
+  done < <(tail -n +2 $CSV)
+}
+
+if [ $# -eq 0 ]; then
+  echo "USAGE: $0 [DIR/FILE]"
+  exit 1
+fi
+
+if test -f $2; then
+  _replace $1 $2
+elif test -d $2; then
+  for F in $2/*; do
+    if test -f $F; then
+      _replace $1 $F
+    fi
+  done
+fi
